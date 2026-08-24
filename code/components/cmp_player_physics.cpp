99eacac2ed253f617837b08c5bc69de726fa4607
markatomniux <mark@omniux.io>
2026-08-24 20:01:01 +0100

    Remove unused logging functionality from PlayerPhysicsComponent to streamline code and improve performance. Clean up includes and eliminate unnecessary dependencies.

--- code/components/cmp_player_physics.cpp
+++ code/components/cmp_player_physics.cpp
@@ -2,8 +2,6 @@
 #include "../code/Controls.h"
 #include <cmath>
 #include <string>
-#include <fstream>
-#include <chrono>
 
 
 using namespace std;
@@ -15,19 +13,6 @@ namespace {
 	// sqrt(gravity) so peak height stays the same while air time drops.
 	constexpr float kJumpGravityScale = 2.f;
 	constexpr float kOldJumpSpeed = 800.f;
-
-	// #region agent log
-	void dbgLog(const char* hid, const char* loc, const char* msg, const string& dataJson)
-	{
-		ofstream f("/Users/markbarton/Documents/BitQuest/.cursor/debug-6bcd73.log", ios::app);
-		if (!f) return;
-		const auto ts = chrono::duration_cast<chrono::milliseconds>(
-			chrono::system_clock::now().time_since_epoch()).count();
-		f << "{\"sessionId\":\"6bcd73\",\"hypothesisId\":\"" << hid
-			<< "\",\"location\":\"" << loc << "\",\"message\":\"" << msg
-			<< "\",\"data\":" << dataJson << ",\"timestamp\":" << ts << "}\n";
-	}
-	// #endregion
 }
 
 bool PlayerPhysicsComponent::isGrounded() const {
@@ -59,10 +44,6 @@ void PlayerPhysicsComponent::update(double dt) {
 
 	if (pos.y > ls::getHeight() * ls::getTileSize())
 	{
-		// #region agent log
-		dbgLog("H4", "cmp_player_physics.cpp:update", "teleport to start",
-			string("{\"py\":") + to_string(pos.y) + ",\"limit\":" + to_string(ls::getHeight() * ls::getTileSize()) + "}");
-		// #endregion
 		teleport(ls::getTilePosition(ls::findTiles(ls::START)[0]));
 	}
 
@@ -120,46 +101,6 @@ void PlayerPhysicsComponent::update(double dt) {
 	v.x = copysign(min(abs(v.x), _maxVelocity.x), v.x);
 	v.y = copysign(min(abs(v.y), _maxVelocity.y), v.y);
 	setVelocity(v);
-
-	// #region agent log
-	{
-		static int dbgN = 0;
-		++dbgN;
-		const bool left = Controls::isPressed("MoveLeft");
-		const bool right = Controls::isPressed("MoveRight");
-		const bool rawA = Keyboard::isKeyPressed(Keyboard::A);
-		const bool rawD = Keyboard::isKeyPressed(Keyboard::D);
-		const bool rawLeft = Keyboard::isKeyPressed(Keyboard::Left);
-		const bool rawRight = Keyboard::isKeyPressed(Keyboard::Right);
-		const bool evtA = Engine::isKeyDown(Keyboard::A);
-		const bool evtD = Engine::isKeyDown(Keyboard::D);
-		const bool evtLeft = Engine::isKeyDown(Keyboard::Left);
-		const bool evtRight = Engine::isKeyDown(Keyboard::Right);
-		if (dbgN <= 5 || (dbgN % 45 == 0) || left || right || rawA || rawD || rawLeft || rawRight || evtA || evtD || evtLeft || evtRight)
-		{
-			dbgLog("H1", "cmp_player_physics.cpp:update", "player physics tick",
-				string("{\"n\":") + to_string(dbgN)
-				+ ",\"left\":" + (left ? "true" : "false")
-				+ ",\"right\":" + (right ? "true" : "false")
-				+ ",\"rawA\":" + (rawA ? "true" : "false")
-				+ ",\"rawD\":" + (rawD ? "true" : "false")
-				+ ",\"rawLeft\":" + (rawLeft ? "true" : "false")
-				+ ",\"rawRight\":" + (rawRight ? "true" : "false")
-				+ ",\"evtA\":" + (evtA ? "true" : "false")
-				+ ",\"evtD\":" + (evtD ? "true" : "false")
-				+ ",\"evtLeft\":" + (evtLeft ? "true" : "false")
-				+ ",\"evtRight\":" + (evtRight ? "true" : "false")
-				+ ",\"canSteer\":" + (canSteer ? "true" : "false")
-				+ ",\"state\":\"" + state + "\""
-				+ ",\"grounded\":" + (_grounded ? "true" : "false")
-				+ ",\"vx\":" + to_string(v.x) + ",\"vy\":" + to_string(v.y)
-				+ ",\"px\":" + to_string(pos.x) + ",\"py\":" + to_string(pos.y)
-				+ ",\"mass\":" + to_string(_body->GetMass())
-				+ ",\"gscale\":" + to_string(_body->GetGravityScale())
-				+ "}");
-		}
-	}
-	// #endregion
 
 	PhysicsComponent::update(dt);
 }
